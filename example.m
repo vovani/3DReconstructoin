1d62f014267e163a5597dac45c40c6daeae89765
Vladimir Anisimov <vovaani@gmail.com>
2016-04-15 15:39:38 +0300

    Added interactive example of the operation

---
+++ example.m
@@ -0,0 +1,23 @@
+function example(A, B, operation)   
+    sld = uicontrol('Style', 'slider',...
+    'Min',-120,'Max',120,'Value',0,...
+    'Position', [20 20 300 20],...
+    'Callback', @callback);
+
+    % Add a text uicontrol to label the slider.
+    sliderValue = get(sld,'Value');
+    txt = uicontrol('Style','text',...
+        'Position',[400 20 120 20],...
+        'String', sliderValue);
+     
+    imshow(B);
+
+
+    function callback(source, ~)
+        t = source.Value / 100.0;
+        set(txt, 'String', num2str(t));
+        fprintf('Calculating %d \n', t);
+        imshow(operation(A, B, t));
+        fprintf('Done\n');
+    end
+end
--- knob.m
+++ knob.m
@@ -1,6 +1,5 @@
 PROJ_BASE_DIR = 'C:\Users\Vladimir Anisimov\Documents\MATLAB\3DReconstruction\';
 
-
 knob_voxelized = false(160,160,300);
 knob_voxelized(30:129, 30:129,:) = VOXELISE(100,100,300,strcat(PROJ_BASE_DIR, 'Door Knob\Door Knob.stl'));
 knob_slices = knob_voxelized(:,:,1:20:300);
