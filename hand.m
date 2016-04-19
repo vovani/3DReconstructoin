bf0b1e2d38e04aa80d58a7e6b9bbc11efd7d1064
Vladimir Anisimov <vovaani@gmail.com>
2016-04-20 00:29:42 +0300

    Enlarged resolution. Significant result improvement

--- hand.m
+++ hand.m
@@ -1,8 +1,8 @@
 PROJ_BASE_DIR = 'C:\Users\Vladimir Anisimov\Documents\MATLAB\3DReconstruction\';
 
-hand_voxelized = false(160,160,300);
-hand_voxelized(30:129, 30:129,:) = VOXELISE(100,100,300,strcat(PROJ_BASE_DIR, 'Hand\Hand.stl'));
-hand_slices = hand_voxelized(:,:,1:8:300);
+hand_voxelized = false(360,360,257);
+hand_voxelized(30:329, 30:329,:) = VOXELISE(300,300,257,strcat(PROJ_BASE_DIR, 'Hand\Hand.stl'));
+hand_slices = hand_voxelized(:,:,1:8:257);
 drawcontour3(hand_slices, 1:size(hand_slices,3));
 for j = 1:3
    hand_slices = four_point(hand_slices, 1/16, @general_average, 1);
--- knob.m
+++ knob.m
@@ -1,8 +1,8 @@
 PROJ_BASE_DIR = 'C:\Users\Vladimir Anisimov\Documents\MATLAB\3DReconstruction\';
 
-knob_voxelized = false(160,160,300);
-knob_voxelized(30:129, 30:129,:) = VOXELISE(100,100,300,strcat(PROJ_BASE_DIR, 'Door Knob\Door Knob.stl'));
-knob_slices = knob_voxelized(:,:,1:8:300);
+knob_voxelized = false(360,360,257);
+knob_voxelized(30:329, 30:329,:) = VOXELISE(300,300,257,strcat(PROJ_BASE_DIR, 'Door Knob\Door Knob.stl'));
+knob_slices = knob_voxelized(:,:,1:8:257);
 drawcontour3(knob_slices, 1:size(knob_slices,3));
 for j = 1:3
    knob_slices = four_point(knob_slices, 1/16, @simply_different_average, 1);
